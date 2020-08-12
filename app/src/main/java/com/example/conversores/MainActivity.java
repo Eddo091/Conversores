624e31c09de1ed34172e8da90787f0f8aec0ba9c
Eddo091 <eddishu0@gmail.com>
2020-08-11 22:45:14 -0600

    22:43h

--- app/src/main/java/com/example/conversores/MainActivity.java
+++ app/src/main/java/com/example/conversores/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.conversores;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+Spinner sptipo;
+Spinner spDe;
+Spinner spA;
+EditText n1;
+    private Object EditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
