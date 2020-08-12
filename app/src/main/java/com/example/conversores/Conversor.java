ef2bec355c8c580e12a7b6b90397c5608683615b
Eddo091 <eddishu0@gmail.com>
2020-08-12 13:19:50 -0600

    Conversor!

--- app/src/main/java/com/example/conversores/Conversor.java
+++ app/src/main/java/com/example/conversores/Conversor.java
@@ -1,4 +1,39 @@
 package com.example.conversores;
 
+import android.widget.TextView;
+
 public class Conversor {
+
+    public String[][] Tipo= new String[][]{
+            new String[]{"Moneda", "Masa", "Volumen", "Longitud", "Almacenamiento", "Tiempo", "Transferencia de datos"}
+    };
+
+    public String[][] Conversor = new String[][]{
+            new String[]{"Dolares", "Euros", "Quetzalez", "Lempira", "Cordobas", "Colon CR", "Dolar Canadience", "Peso Chileno", "Peso Arg", "Yen JPY", "Won SurCoreano", "Won Norcoreano", "MXN", "Rupia India", "Corona Danesa", "Dirham Marroqui", "Escudo caboverdiano", "Pula botsuanés", "Dolar bahameño", "Corona noruega"}, //monedas
+            new String[]{"Metros", "CM", "Pulgadas", "Pie", "Varas", "Yardas", "Micrometro", "Nanometro"}, //Longitud
+            new String[]{"Libra", "Gramos", "Kilogramos", "Onzas", "Quintales", "Toneladas", "Tonelada larga", "Tonelada Corta"}, //Peso
+            new String[]{"Libra", "Mililitro", "Galon imperial", "Cuarto imperial", "Pinta imperial", "Taza Imperial", "Onza liquida imperial"}, //Volumen
+            new String[]{"Bit", "Byte", "Kilobyte", "Megabyte", "Gygabyte", "Terabyte", "Petabyte"} //Almacenamiento
+    };
+    double[][] valores = {
+            //monedas
+            new double[]{1,0.92,7.65,24.9,34.22,572.26,1.33458,813.003,62.0138, 110.315, 1216.21, 900.072, 19.3635, 71.6572, 6.85392, 9.68004, 101.144, 11.0795, 1.00000, 9.39713},
+            //Longitud
+            new double[]{1,100,39.37,3.281,1.196,1.094,1e+6,1e+9},
+            //Peso
+            new double[]{1, 453.592, 0.453592, 16, 0.000453592,0.000453592,0.000446429,0.0005},
+            //Volumen
+            new double[]{1,1000, 0.219969, 0.879877, 1.759752,3.519504309673087,35.1950799988562},
+            //Almacenamiento
+            new double[]{1, 0.125, 0.000125, 1.25e-7, 1.25e-10 , 1.25e-13, 1.25e-16 }
+    };
+  /* public double convertir(int opcion, int de, int a, String cantidad)
+    {
+        //double v = (valores[opcion][a] / valores[opcion][de]) * cantidad;
+        //return v;
+    }
+
+    //Intenté hacerlo por clases, pero no supe como seguirlo en el mainactivity
+
+   */
 }
--- app/src/main/java/com/example/conversores/MainActivity.java
+++ app/src/main/java/com/example/conversores/MainActivity.java
@@ -4,23 +4,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+Conversor ObjConversor  = new Conversor();
 
 Spinner sptipo;
 Spinner spDe;
 Spinner spA;
 EditText n1;
+TextView lblRes;
+Button BtnConv;
+
     private Object EditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        //Definir
+        n1 = (EditText) findViewById( R.id.num1 );
+        lblRes = (TextView) findViewById( R.id.lblRespuesta );
+        sptipo = (Spinner) findViewById( R.id.cboTipo );
+        spA = (Spinner) findViewById( R.id.cboA );
+        spDe = (Spinner) findViewById( R.id.cboDe );
+        BtnConv = (Button) findViewById( R.id.BtnConvertir );
+        ArrayAdapter<String> Tip;
+        ArrayAdapter<String> de;
+        ArrayAdapter<String> a;
+        String Tipo= String.valueOf( ObjConversor.Tipo );
+        String De= String.valueOf( ObjConversor.Conversor );
+        String A= String.valueOf( ObjConversor.Conversor );
+        sptipo.setOnItemClickListener( (AdapterView.OnItemClickListener) this );
+        spA.setOnItemClickListener( (AdapterView.OnItemClickListener) this );
+        spDe.setOnItemClickListener( (AdapterView.OnItemClickListener) this );
+        Tip= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, Collections.singletonList( Tipo ) );
+        de= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, Collections.singletonList( De ) );
+        a= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, Collections.singletonList( A) );
 
+        sptipo.setAdapter( Tip );
+        spDe.setAdapter(de );
+        spA.setAdapter( a );
+
+
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> a, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
