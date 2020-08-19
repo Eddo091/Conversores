cf301f0b2b2c5b6229a2c998f13e0304edb278d3
Eddo091 <eddishu0@gmail.com>
2020-08-19 12:43:55 -0600

    Inserté los valores de los tipos de conversores, no sé si está bien, ya que para el lblrespuesta, no supe como mandarle a mostrar en pantalla, aún trato de entender el nuevo lenguaje

--- app/src/main/java/com/example/conversores/MainActivity.java
+++ app/src/main/java/com/example/conversores/MainActivity.java
@@ -9,17 +9,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
-import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
-    String[][] mon = {
+    String[][] tipos = {
             {"Dolar","Euro","Colon SV", "Colon CR","Lempira","Bitcoin","Won Sur Coreano","Won Nor Coreano","Peso ARG","Peso MXN"},//Moneda
             {"Gramo","Kilogramo","Miligramo", "Microgramo", "Tonelada Larga","Tonelada corta","Libra","Onza","Tonelada","Stone"},//Masa
             {"Litro","Mililitro","Galón imperial", "Cuarto imperial","Pinta imperial", "Taza imperial","Cucharada imperial","Cucharadita imperial","Pie cúbico","Pulgada cúbica"},//Volumen
@@ -27,7 +21,19 @@ public class MainActivity extends AppCompatActivity {
             {"Bit","Byte","KB", "MB","GB", "TB","PB","EB","ZB","YB"},//Almacenamiento
             {"Sg","Nanosg","Minuto", "Hora","Día", "Semana","Mes","Año Natural","Década","Siglo"},//Tiempo
             {"Bit por sg","Kbit por sg","KB por sg", "MGBit por sg","MG por sg", "MiBit por sg","GBit por sg","GB por sg","Gibit por sg","TB por sg"},//Transferencia de datos
-    }; //Save
+    };
+    double[][] valores={
+            new double[]{1,0.84,8.75,594.93,24.65,0.000085,1180.54,900.045,73.48,22.02},//moneda
+            new double[]{1,0.001,1000,1e+6,9.8421e-7,1.1023e-6,0.00220462,0.035274,1e-6,0.000157473},//Masa
+            new double[]{1,1000,0.219969,0.879876,1.75975,3.51951,56.3121,168.936,0.0353147,61.0237},//Volumen
+            new double[]{1,100,1000,1e+6,1e+9,0.000621371,1.09361,3.28084,39.3701,0.000539957},//Longitud
+            new double[]{1,0.125,0.000125,1.25e-7,1.25e-10,1.25e-13,1.25e-16,1.25e-19,1.25e-22,1.25e-25},//Almacenamiento
+            new double[]{1,1e+9,0.0166667,0.000277778,1.1574083333e-5,1.653440476142857e-6,3.80517391202858972e-7,3.170981735068493655e-8,3.171e-9,3.171e-10},//Tiempo
+            new double[]{1,0.001,0.000125,1e-6,1.25e-7,9.5367e-7,1e-9,1.25e-10,9.3132e-10,1e-12}//Transferencia de datos
+
+    };
+
+    //Save
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +46,12 @@ public class MainActivity extends AppCompatActivity {
         final TextView lblresp=(TextView)findViewById( R.id.lblRespuesta );
         final Button Convertir=(Button)findViewById( R.id.BtnConvertir );
         //Spinner
-        Spinner sptipo =(Spinner)findViewById( R.id.cboTipo );
+        final Spinner sptipo =(Spinner)findViewById( R.id.cboTipo );
         sptipo.setOnItemSelectedListener( new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-            spDe.setAdapter( new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, mon[position]  ) );
-            spA.setAdapter( new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, mon[position]  ) );
+            spDe.setAdapter( new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, tipos[position]  ) );
+            spA.setAdapter( new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, tipos[position]  ) );
             }
 
             @Override
@@ -53,10 +59,19 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
+            public double procesar(){
+                boolean retorno= true;
+                int opcion = 0,de=0,a = 0;
+                double cantidad = 0;
+
+                return valores[opcion][a]/valores[opcion][de]*cantidad;
+            };
             //Onclick_codigo
             public  void ProcesarBtncon (View c)
             {
                 validar();
+                procesar();
+
 
             }
             //Validación de texto
@@ -73,13 +88,16 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
-            //Valores de conversor
-            
+
+
+
 
         } );
+
     }
 //Onclick_Accion
     public void ProcesarBtnConvertir(View view) {
        ProcesarBtnConvertir( view );
+
     }
 }
